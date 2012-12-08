a9fd4fb54b7c3c2d4dfcb3fb34c07bc217033722
comradea_cp <SND\comradea_cp>
2012-12-08 09:23:09 +0000

    Refactor Log methods

---
+++ MD5Verifier/MD5Verifier/Enums.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MD5FolderVerifier
+{
+    enum LogMsgType
+    {
+        Info,
+        Error,
+        Warning,
+    }
+
+    enum RunningMode
+    {
+        Console,
+        GUI,
+    }
+
+    enum LogResultType
+    {
+        GenerateSuccess,
+        NotFound,
+        Passed,
+        Mismatch,
+        AlreadyExist,
+    }
+}
