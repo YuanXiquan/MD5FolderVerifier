4efb53a0f90352aeb6e4bba32b943f7901dbbdeb
Xiquan Yuan <yuanxiquan@gmail.com>
2023-12-28 00:03:25 +0800

    Remove String Build in MD5 calculation.

--- MD5Verifier/MD5Verifier/MD5ChecksumVerifier.cs
+++ MD5Verifier/MD5Verifier/MD5ChecksumVerifier.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.IO;
 using System.Security.Cryptography;
 using System.Configuration;
+using System.Security.Policy;
 
 namespace MD5FolderVerifier
 {
@@ -302,18 +303,13 @@ namespace MD5FolderVerifier
         private string GetMD5HashFromFile(string fileName)
         {
             MD5 md5 = new MD5CryptoServiceProvider();
-            byte[] retVal;
+            byte[] hashArray;
             using (FileStream file = new FileStream(fileName, FileMode.Open))
             {
-                retVal = md5.ComputeHash(file);
+                hashArray = md5.ComputeHash(file);
             }
 
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < retVal.Length; i++)
-            {
-                sb.Append(retVal[i].ToString("x2"));
-            }
-            return sb.ToString();
+            return BitConverter.ToString(hashArray).Replace("-", String.Empty).ToLowerInvariant();
         }
         #endregion
 
